caa473394e6599806a13498fd09c660f5028b155
Pranav <pranavtrivedi02@gmail.com>
2023-05-02 23:56:34 +0530

    added new file

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,9 @@ type PDFFile = string | File | null;
 
 export default function App() {
   const [file, setFile] = useState<PDFFile>('./sample.pdf');
+  const [file1, setFile1] = useState<PDFFile>('./exp_1.pdf');
   const [numPages, setNumPages] = useState<number>();
+  const [numPages1, setNumPages1] = useState<number>();
 
   useEffect(() => {
     pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
@@ -31,6 +33,7 @@ export default function App() {
 
   function onDocumentLoadSuccess({ numPages: nextNumPages }: PDFDocumentProxy) {
     setNumPages(nextNumPages);
+    setNumPages1(nextNumPages);
   }
 
   return (
@@ -44,6 +47,11 @@ export default function App() {
           <input onChange={onFileChange} type="file" />
         </div>
         <div className="Example__container__document">
+        <Document file={file1} onLoadSuccess={onDocumentLoadSuccess} options={options}>
+            {Array.from(new Array(numPages1), (el, index) => (
+              <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+            ))}
+          </Document>
           <Document file={file} onLoadSuccess={onDocumentLoadSuccess} options={options}>
             {Array.from(new Array(numPages), (el, index) => (
               <Page key={`page_${index + 1}`} pageNumber={index + 1} />
